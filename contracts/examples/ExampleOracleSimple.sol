58557054ad8cccd699e64198813718fdc83209ef
CoreyLin <514971757@qq.com>
2023-02-01 16:06:59 +0800

    add comment

--- contracts/examples/ExampleOracleSimple.sol
+++ contracts/examples/ExampleOracleSimple.sol
@@ -10,29 +10,33 @@ import '../libraries/UniswapV2Library.sol';
 // fixed window oracle that recomputes the average price for the entire period once every period
 // note that the price average is only guaranteed to be over at least 1 period, but may be over a longer period
 contract ExampleOracleSimple {
-    using FixedPoint for *;
+    using FixedPoint for *; // *这种语法注意一下，值得学习
 
-    uint public constant PERIOD = 24 hours;
+    uint public constant PERIOD = 24 hours; // 一个周期是24小时
 
-    IUniswapV2Pair immutable pair;
-    address public immutable token0;
-    address public immutable token1;
+    IUniswapV2Pair immutable pair; // 取哪个pair的价格
+    address public immutable token0; // pair的token0地址
+    address public immutable token1; // pair的token1地址
 
     uint    public price0CumulativeLast;
     uint    public price1CumulativeLast;
     uint32  public blockTimestampLast;
-    FixedPoint.uq112x112 public price0Average;
-    FixedPoint.uq112x112 public price1Average;
+    FixedPoint.uq112x112 public price0Average; // token0的平均价格，注意是一个public方法
+    FixedPoint.uq112x112 public price1Average; // token1的平均价格，注意是一个public方法
 
     constructor(address factory, address tokenA, address tokenB) public {
+        // 根据tokenA,tokenB找到对应的pair，然后初始化IUniswapV2Pair合约
         IUniswapV2Pair _pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
         pair = _pair;
         token0 = _pair.token0();
         token1 = _pair.token1();
+        // 得到当前accumulated price value (1 / 0)
         price0CumulativeLast = _pair.price0CumulativeLast(); // fetch the current accumulated price value (1 / 0)
+        // 得到当前accumulated price value (0 / 1)
         price1CumulativeLast = _pair.price1CumulativeLast(); // fetch the current accumulated price value (0 / 1)
         uint112 reserve0;
         uint112 reserve1;
+        // 得到当前reserve0, reserve1以及最近一次更新accumulated price的区块的时间戳
         (reserve0, reserve1, blockTimestampLast) = _pair.getReserves();
         require(reserve0 != 0 && reserve1 != 0, 'ExampleOracleSimple: NO_RESERVES'); // ensure that there's liquidity in the pair
     }
@@ -40,16 +44,20 @@ contract ExampleOracleSimple {
     function update() external {
         (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) =
             UniswapV2OracleLibrary.currentCumulativePrices(address(pair));
+        // 当前区块时间戳离上次blockTimestampLast的时间差，注意，本方法的最后一行代码会把当前时间戳又设置为blockTimestampLast，即更新blockTimestampLast
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
 
         // ensure that at least one full period has passed since the last update
+        // 时间流逝必须超过至少一个周期，即24小时
         require(timeElapsed >= PERIOD, 'ExampleOracleSimple: PERIOD_NOT_ELAPSED');
 
         // overflow is desired, casting never truncates
         // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+        // 计算价格
         price0Average = FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
         price1Average = FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
 
+        // 更新状态变量
         price0CumulativeLast = price0Cumulative;
         price1CumulativeLast = price1Cumulative;
         blockTimestampLast = blockTimestamp;
--- contracts/libraries/UniswapV2OracleLibrary.sol
+++ contracts/libraries/UniswapV2OracleLibrary.sol
@@ -8,27 +8,34 @@ library UniswapV2OracleLibrary {
     using FixedPoint for *;
 
     // helper function that returns the current block timestamp within the range of uint32, i.e. [0, 2**32 - 1]
+    // helper函数，返回uint32范围内的当前区块时间戳，即[0,2 **32 - 1]之间
     function currentBlockTimestamp() internal view returns (uint32) {
         return uint32(block.timestamp % 2 ** 32);
     }
 
     // produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
+    // 使用反事实生成当前区块的累计价格以节省gas并避免调用sync方法。
     function currentCumulativePrices(
         address pair
     ) internal view returns (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) {
+        // 获取当前区块的时间戳，注意：原始时间戳除以2**32之后取余
         blockTimestamp = currentBlockTimestamp();
+        // 获取最新的cumulative price
         price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
         price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
 
         // if time has elapsed since the last update on the pair, mock the accumulated price values
         (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
-        if (blockTimestampLast != blockTimestamp) {
+        if (blockTimestampLast != blockTimestamp) { // 说明当前区块的时间戳大于最近一次更新cumulative price的时间戳
             // subtraction overflow is desired
+            // 当前区块的时间戳 - cumulative price的时间戳
             uint32 timeElapsed = blockTimestamp - blockTimestampLast;
             // addition overflow is desired
             // counterfactual
+            // 当前价格reserve1/reserve0，然后乘以时间
             price0Cumulative += uint(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsed;
             // counterfactual
+            // 当前价格reserve0/reserve1，然后乘以时间
             price1Cumulative += uint(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsed;
         }
     }
